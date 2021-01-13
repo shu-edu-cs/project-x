49d28bfc7e9d2502c69d512b1a25df6524100d1f
xp.huang <76925945+shu-edu@users.noreply.github.com>
2021-01-13 21:10:52 +0800

    数据库切换至MySQL

--- market/models.py
+++ market/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from datetime import datetime
 from django.contrib import admin
+from django.conf import settings
 
 
 class BaseModel(models.Model):
 
+    class Meta:
+        abstract = True
+
+    create_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None, verbose_name='创建用户')
     create_time = models.DateTimeField(auto_now_add=True, verbose_name='创建时间')
     update_time = models.DateTimeField(auto_now=datetime.now(), verbose_name='更新时间')
 
@@ -12,6 +17,7 @@ class BaseModel(models.Model):
 class Book(BaseModel):
 
     class Meta:
+        db_table = 'market_book'
         verbose_name = '共享图书'
         verbose_name_plural = verbose_name
 
@@ -27,14 +33,57 @@ class Book(BaseModel):
     status_type = ((0, '待出借'), (1, '出借中'))
     status = models.SmallIntegerField(null=False, verbose_name='状态', choices=status_type)
     remark = models.CharField(max_length=512, null=True, verbose_name='备注')
+    create_user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None,
+                                    verbose_name='共享人')
 
     def __str__(self):
         return self.book_name
 
 
+class MyBook(Book):
+
+    class Meta:
+        proxy = True
+        verbose_name = '我的共享'
+        verbose_name_plural = verbose_name
+
+
 @admin.register(Book)
-class StudentAdmin(admin.ModelAdmin):
+class BookAdmin(admin.ModelAdmin):
+    list_filter = ('status', )
+    search_fields = ('book_num', 'book_name', 'author')
+    list_display = ('book_num', 'book_name', 'author', 'status', 'create_user')
+    fields = ('book_num', 'book_name', 'book_isbn', 'public_name', 'publish_year', 'author', 'translator', 'edition_order',
+              'page_count',  'status', 'remark')
+    list_per_page = 20
+
+    def get_queryset(self, request):
+        if not request.user.is_superuser:
+            return Book.objects.filter(status=0)
+
+    def save_model(self, request, obj, form, change):
+        if getattr(obj, 'create_user', None) is None:
+            obj.create_user = request.user
+        obj.save()
+
+    def borrow_book(self, request, queryset):
+        pass
+
+    borrow_book.short_description = '借书'
+    borrow_book.icon = 'fas el-icon-notebook-2'
+    borrow_book.type = 'success'
+
+    actions = ['borrow_book']
+
+
+@admin.register(MyBook)
+class MyBookAdmin(admin.ModelAdmin):
     list_filter = ('status', )
     search_fields = ('book_num', 'book_name', 'author')
     list_display = ('book_num', 'book_name', 'author', 'status')
+    fields = ('book_num', 'book_name', 'book_isbn', 'public_name', 'publish_year', 'author', 'translator', 'edition_order',
+              'page_count',  'status', 'remark')
     list_per_page = 20
+
+    def get_queryset(self, request):
+        return Book.objects.filter(create_user=request.user)
